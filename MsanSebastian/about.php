a360003ca17bf39d353ddf35de7dd1a74fb016d5
josemunoz <jose.e.munoz.nunez@gmail.com>
2025-12-07 12:25:01 -0300

    Content: Translate about.php to Spanish and replace Lorem text

--- MsanSebastian/about.php
+++ MsanSebastian/about.php
@@ -6,11 +6,11 @@
             <div class="content-box">
                 <div class="content-wrapper">
                     <div class="title">
-                        <h1>About</h1>
+                        <h1>Sobre Nosotros</h1>
                     </div>
                     <ul class="bread-crumb">
-                        <li><a href="index.php">Home</a></li>
-                        <li>About</li>
+                        <li><a href="index.php">Inicio</a></li>
+                        <li>Sobre Nosotros</li>
                     </ul>
                 </div>                    
             </div>
@@ -27,7 +27,7 @@
                             <img src="assets/images/resource/image-8.jpg" alt="">
                             <div class="experience-years">
                                 <div class="icon"><span class="fab fa-twitter"></span></div>
-                                <h4>36 Years of <br> Expericence</h4>
+                                <h4>Años de <br> Experiencia</h4>
                             </div>
                         </div>
                     </div>
@@ -35,23 +35,23 @@
                 <div class="col-lg-6">
                     <div class="content-side">
                         <div class="sec-title mb-40">
-                            <div class="sub-title">Welcome to Welbim</div>
-                            <h2>We’re Committed <br> To quality</h2>
+                            <div class="sub-title">Bienvenido a Maestranza San Sebastián</div>
+                            <h2>Comprometidos con <br> la Calidad</h2>
                         </div>
-                        <div class="text mb-35">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. quis nostrud exercitation.</div>
+                        <div class="text mb-35">Somos una empresa especializada en trabajos metálicos con años de experiencia en el sector. Ofrecemos soluciones integrales para proyectos industriales, comerciales y residenciales, garantizando calidad, seguridad y cumplimiento de plazos.</div>
                         <div class="bottom-content">
 
                             <div class="row">
                                 <div class="col-md-6">
                                     <ul class="list">
-                                        <li>We provide 24/7 service</li>
-                                        <li>We strick to deadline</li>
+                                        <li>Atención personalizada</li>
+                                        <li>Cumplimiento de plazos</li>
                                     </ul>
                                 </div>
                                 <div class="col-md-6">
                                     <ul class="list">
-                                        <li>We offer upfront pricing</li>
-                                        <li>We use latest technology</li>
+                                        <li>Presupuestos claros</li>
+                                        <li>Tecnología moderna</li>
                                     </ul>
                                 </div>
                             </div>
@@ -59,7 +59,7 @@
                             <div class="progress-levels">
                                 <!--Skill Box-->
                                 <div class="progress-box wow fadeIn" data-wow-delay="100ms" data-wow-duration="1500ms">
-                                    <h5>Welding</h5>
+                                    <h5>Soldadura</h5>
                                     <div class="inner">
                                         <div class="bar">
                                             <div class="bar-innner"><div class="bar-fill" data-percent="91"><div class="percent"></div></div></div>
@@ -68,7 +68,7 @@
                                 </div>
                                 <!--Skill Box-->
                                 <div class="progress-box wow fadeIn" data-wow-delay="100ms" data-wow-duration="1500ms">
-                                    <h5>Metal Work</h5>
+                                    <h5>Trabajos Metálicos</h5>
                                     <div class="inner">
                                         <div class="bar">
                                             <div class="bar-innner"><div class="bar-fill" data-percent="78"><div class="percent"></div></div></div>
@@ -77,7 +77,7 @@
                                 </div>
                                 <!--Skill Box-->
                                 <div class="progress-box wow fadeIn" data-wow-delay="100ms" data-wow-duration="1500ms">
-                                    <h5>Steel Weld</h5>
+                                    <h5>Estructuras de Acero</h5>
                                     <div class="inner">
                                         <div class="bar">
                                             <div class="bar-innner"><div class="bar-fill" data-percent="64"><div class="percent"></div></div></div>
@@ -111,16 +111,16 @@
     <section class="testimonials-section-two style-two" style="background-image: url(assets/images/resource/map.png)">
         <div class="auto-container">
             <div class="sec-title text-center">
-                <div class="sub-title text-center">Testimonials</div>
-                <h2>What they say</h2>
+                <div class="sub-title text-center">Testimonios</div>
+                <h2>Lo que dicen nuestros clientes</h2>
             </div>
             <div class="row">
                 <div class="theme_carousel owl-theme owl-carousel" data-options='{"loop": true, "margin": 0, "autoheight":true, "lazyload":true, "nav": true, "dots": true, "autoplay": true, "autoplayTimeout": 6000, "smartSpeed": 1000, "responsive":{ "0" :{ "items": "1" }, "600" :{ "items" : "1" }, "768" :{ "items" : "2" } , "992":{ "items" : "2" }, "1200":{ "items" : "3" }}}'>
                     <div class="testimonial-block-two">
                         <div class="inner-box">                            
-                            <div class="text">There are many variations of passage of lorem ipsum but the majority alteration in some form, by rando mised words noted digital simply look.
+                            <div class="text">Excelente trabajo en la fabricación de nuestra estructura metálica. Profesionalismo y calidad garantizada. Los recomiendo ampliamente.
                             </div>
-                            <h4>Jessica Brown <span>Co-Founder</span></h4>
+                            <h4>María González <span>Arquitecta</span></h4>
                             <div class="author-thumb">
                                 <img src="assets/images/resource/author-1.jpg" alt="">
                             </div>
@@ -128,9 +128,9 @@
                     </div>
                     <div class="testimonial-block-two">
                         <div class="inner-box">                            
-                            <div class="text">There are many variations of passage of lorem ipsum but the majority alteration in some form, by rando mised words noted digital simply look.
+                            <div class="text">Servicio rápido y eficiente. Repararon la carrocería de nuestro camión en tiempo récord. Muy satisfecho con el resultado.
                             </div>
-                            <h4>John Smith  <span>Co-Founder</span></h4>
+                            <h4>Juan Pérez <span>Empresa de Transporte</span></h4>
                             <div class="author-thumb">
                                 <img src="assets/images/resource/author-2.jpg" alt="">
                             </div>
@@ -138,9 +138,9 @@
                     </div>
                     <div class="testimonial-block-two">
                         <div class="inner-box">                            
-                            <div class="text">There are many variations of passage of lorem ipsum but the majority alteration in some form, by rando mised words noted digital simply look.
+                            <div class="text">Trabajos de soldadura impecables. Personal capacitado y precios justos. Definitivamente volveremos a trabajar con ellos.
                             </div>
-                            <h4>Christine eve <span>Co-Founder</span></h4>
+                            <h4>Carolina Rojas <span>Constructora</span></h4>
                             <div class="author-thumb">
                                 <img src="assets/images/resource/author-3.jpg" alt="">
                             </div>
@@ -159,10 +159,10 @@
                 <div class="col-xl-7 offset-xl-5">
                     <div class="wrapper-box">
                         <div class="sec-title light">
-                            <div class="sub-title">Our Fun Facts</div>
-                            <h2>Dream it, We can <br>Build it</h2>
+                            <div class="sub-title">Nuestros Números</div>
+                            <h2>Soñalo, Nosotros <br>Lo Construimos</h2>
                         </div>
-                        <div class="text">There are many variations of passages of Lorem Ipsum but the majority is have suffered alteration in some form, or randomised words which don't look even slightly believable.</div>
+                        <div class="text">Con años de trayectoria en el sector metalúrgico, hemos completado cientos de proyectos exitosos. Nuestro compromiso con la excelencia nos ha convertido en la opción preferida para trabajos metálicos en Santiago y sus alrededores.</div>
                         <div class="facts-area">
                             <div class="row">
                                 <!--Column-->
@@ -172,7 +172,7 @@
                                             <div class="count-outer count-box">
                                                 <span class="count-text" data-speed="3000" data-stop="800">0</span>
                                             </div>
-                                            <div class="fact-text"> Projects <br> Completed</div>
+                                            <div class="fact-text">Proyectos <br> Completados</div>
                                         </div>
                                     </div>
                                 </div>
@@ -183,7 +183,7 @@
                                             <div class="count-outer count-box">
                                                 <span class="count-text" data-speed="3000" data-stop="681">0</span>
                                             </div>
-                                            <div class="fact-text"> Satisfied <br> Clients</div>
+                                            <div class="fact-text">Clientes <br> Satisfechos</div>
                                         </div>
                                     </div>
                                 </div>
@@ -194,7 +194,7 @@
                                             <div class="count-outer count-box">
                                                 <span class="count-text" data-speed="3000" data-stop="909">0</span>
                                             </div>
-                                            <div class="fact-text">Metal <br> Works</div>
+                                            <div class="fact-text">Trabajos <br> Metálicos</div>
                                         </div>
                                     </div>
                                 </div>
@@ -210,16 +210,16 @@
     <section class="team-section">
         <div class="auto-container">
             <div class="sec-title text-center">
-                <div class="sub-title">Our Services</div>
-                <h2>What we offer</h2>
+                <div class="sub-title">Nuestro Equipo</div>
+                <h2>Profesionales Expertos</h2>
             </div>
             <div class="row">   
                 <div class="col-lg-4 col-md-6 team-block">
                     <div class="inner-box wow fadeInUp" data-wow-duration="1500ms">
                         <div class="image"><img src="assets/images/resource/team-1.jpg" alt=""></div>
                         <div class="content">
-                            <h4>Robert Smith</h4>
-                            <div class="designation">Co Founder</div>
+                            <h4>Roberto González</h4>
+                            <div class="designation">Maestro Soldador</div>
                             <ul class="social-icon">
                                 <li><a href="#"><i class="fab fa-twitter"></i></a></li>
                                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
@@ -232,8 +232,8 @@
                     <div class="inner-box wow fadeInDown" data-wow-duration="1500ms">
                         <div class="image"><img src="assets/images/resource/team-2.jpg" alt=""></div>
                         <div class="content">
-                            <h4>Christine eve</h4>
-                            <div class="designation">Welder</div>
+                            <h4>María Silva</h4>
+                            <div class="designation">Especialista en Estructuras</div>
                             <ul class="social-icon">
                                 <li><a href="#"><i class="fab fa-twitter"></i></a></li>
                                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
@@ -246,8 +246,8 @@
                     <div class="inner-box wow fadeInUp" data-wow-duration="1500ms">
                         <div class="image"><img src="assets/images/resource/team-3.jpg" alt=""></div>
                         <div class="content">
-                            <h4>Mike Hardson</h4>
-                            <div class="designation">Mechanic</div>
+                            <h4>Miguel Herrera</h4>
+                            <div class="designation">Jefe de Taller</div>
                             <ul class="social-icon">
                                 <li><a href="#"><i class="fab fa-twitter"></i></a></li>
                                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
@@ -264,9 +264,9 @@
     <section class="cta-section">
         <div class="auto-container">
             <div class="wrapper-box">
-                <h3>Looking for a quality and <br>affordable project?</h3>
+                <h3>¿Buscas calidad y <br>precios competitivos?</h3>
                 <div class="link">
-                    <a href="#" class="theme-btn btn-style-one"><span>Contact With Us</span></a>
+                    <a href="contact.php" class="theme-btn btn-style-one"><span>Contáctanos</span></a>
                 </div>
             </div>
         </div>
